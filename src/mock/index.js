1ee038053b4f73c94a78433903a6e2053befc527
russelljumpdigital <77699151+russelljumpdigital@users.noreply.github.com>
2022-03-14 14:15:27 +0000

    test

--- src/mock/index.js
+++ src/mock/index.js
@@ -28,6 +28,35 @@ export const users = [
     login: { email: "", password: "" },
     status: { type: "admin", blocked: false, lastLoginTimestamp: 0 },
   },
+  {
+    userId: 1,
+    signUpDate: 0,
+    personalDetails: {
+      name: { firstName: "", lastName: "" },
+      dob: { year: "", months: "", day: "" },
+      location: { town: "", postCode: "" },
+      kids: false,
+      religion: "",
+      height: 0,
+      gender: 0, //0 is male, 1 is female, 2 is other
+      smokers: false,
+    },
+    preferences: {
+      lifeStyle: { marriage: false, casual: false, openToKids: false },
+      minAge: 0,
+      maxAge: 0,
+      acceptedReligions: ["", ""],
+      height: { min: 0, max: 0 },
+      gender: 0, //0 is male, 1 is female, 2 is other
+      kidsAccepted: false,
+      smokers: false,
+    },
+    matches: [], //ids
+    likes: [], //ids
+    blocked: [], //ids
+    login: { email: "", password: "" },
+    status: { type: "admin", blocked: false, lastLoginTimestamp: 0 },
+  },
 ];
 
 export const messages = [
