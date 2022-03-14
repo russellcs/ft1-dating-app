fd5a6b47c8b605542e3081ddc51fcd5b331806e4
Frederick Horne <frederickhorne@Fredericks-MacBook-Pro.local>
2022-03-14 22:14:36 +0000

    added toggling like and block buttons

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,5 +1,49 @@
-const Messages = () => {
-  return <>M</>;
+import { useState } from "react";
+
+const Messages = (props) => {
+	const [draft, setDraft] = useState();
+	const [message, setMessage] = useState();
+	const [likeClicked, setLikeClicked] = useState(false);
+	const [blockClicked, setBlockClicked] = useState(false);
+
+	const onInput = (e) => setDraft(e.target.value);
+
+	const displayMessage = () => {
+		setMessage(draft);
+	};
+
+	const onKeyDown = (e) => {
+		if (e.key === "Enter") {
+			displayMessage();
+		}
+	};
+
+	const isLikeClicked = () => {
+		setLikeClicked(likeClicked === false ? true : false);
+	};
+
+	const blockUser = () => {
+		setBlockClicked(blockClicked === false ? true : false);
+	};
+
+	const blockButtonText = blockClicked ? "Unblock user" : "Block user";
+
+	return (
+		<>
+			<div className="message">
+				{message}
+				{message && <button onClick={isLikeClicked}>&#9829;</button>}
+				{likeClicked && <p>Message liked</p>}
+			</div>
+
+			<input onInput={onInput} onKeyDown={onKeyDown} type="text" />
+
+			<button onClick={displayMessage}>Send</button>
+			<br />
+			{/* <button>Block {props.users.personalDetails.name.firstName}</button> */}
+			<button onClick={blockUser}>{blockButtonText}</button>
+		</>
+	);
 };
 
 export default Messages;
--- src/mock/index.js
+++ src/mock/index.js
@@ -1,42 +1,42 @@
 export const users = [
-  {
-    userId: 1,
-    signUpDate: 0,
-    personalDetails: {
-      name: { firstName: "", lastName: "" },
-      dob: { year: "", months: "", day: "" },
-      location: { town: "", postCode: "" },
-      kids: false,
-      religion: "",
-      height: 0,
-      gender: 0, //0 is male, 1 is female, 2 is other
-      smokers: false,
-    },
-    preferences: {
-      lifeStyle: { marriage: false, casual: false, openToKids: false },
-      minAge: 0,
-      maxAge: 0,
-      acceptedReligions: ["", ""],
-      height: { min: 0, max: 0 },
-      gender: 0, //0 is male, 1 is female, 2 is other
-      kidsAccepted: false,
-      smokers: false,
-    },
-    matches: [], //ids
-    likes: [], //ids
-    blocked: [], //ids
-    login: { email: "", password: "" },
-    status: { type: "admin", blocked: false, lastLoginTimestamp: 0 },
-  },
+	{
+		userId: 1,
+		signUpDate: 0,
+		personalDetails: {
+			name: { firstName: "John", lastName: "" },
+			dob: { year: "", months: "", day: "" },
+			location: { town: "", postCode: "" },
+			kids: false,
+			religion: "",
+			height: 0,
+			gender: 0, //0 is male, 1 is female, 2 is other
+			smokers: false,
+		},
+		preferences: {
+			lifeStyle: { marriage: false, casual: false, openToKids: false },
+			minAge: 0,
+			maxAge: 0,
+			acceptedReligions: ["", ""],
+			height: { min: 0, max: 0 },
+			gender: 0, //0 is male, 1 is female, 2 is other
+			kidsAccepted: false,
+			smokers: false,
+		},
+		matches: [], //ids
+		likes: [], //ids
+		blocked: [], //ids
+		login: { email: "", password: "" },
+		status: { type: "admin", blocked: false, lastLoginTimestamp: 0 },
+	},
 ];
 
 export const messages = [
-  {
-    userId: 1,
-    messageId: 1,
-    content: "",
-    sendTimestamp: 0,
-    read: false,
-    blocked: false,
-  },
+	{
+		userId: 1,
+		messageId: 1,
+		content: "",
+		sendTimestamp: 0,
+		read: false,
+		blocked: false,
+	},
 ];
