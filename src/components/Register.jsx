4a8619b6f4f5c0ad11d750e80c7cb9a5e9fee9cf
TF <tf@tfs-imac.lan>
2022-03-15 17:32:52 +0000

    joi and forms

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,27 +1,53 @@
 import { useState } from "react";
+import {
+  religions,
+  wantKids,
+  smokes,
+  genders,
+  schema,
+  joiDataReorder,
+} from "../config/formConfig";
 import "./register.css";
+import Joi from "joi";
 
 const Register = (props) => {
-  // console.log(props.users);
-
   let [data, setData] = useState({});
-
-  const myForm = document.forms["registerForm"];
-  const formValues = new FormData(myForm);
-  const dataObject = Object.fromEntries(formValues);
-  console.log(dataObject);
+  let [errors, setErrors] = useState({});
 
   const onSubmit = () => {
     //will send the data to be added to users
   };
 
   const onInput = (e) => {
-    setData({ ...data, [e.target.name]: e.target.value });
+    let value = e.target.value;
+    if (e.target.name === "dateOfBirth") {
+      value = new Date(e.target.value).getTime();
+    }
+    const newState = { ...data, [e.target.name]: value };
+    setData(newState);
+    onValidate(newState);
   };
 
+  const onValidate = async (data) => {
+    const _joiInstance = Joi.object(schema);
+    try {
+      await _joiInstance.validateAsync(data);
+      setErrors({ errors: "" });
+    } catch (errors) {
+      setErrors({ errors: joiDataReorder(errors.details) });
+    }
+  };
+
+  console.log(data);
+  console.log(errors);
   return (
     <>
-      <form onInput={onInput} onSubmit={onSubmit} name="registerForm">
+      <form
+        className="form"
+        onInput={onInput}
+        onSubmit={onSubmit}
+        name="registerForm"
+      >
         <h1>Please enter your details</h1>
         <div className="formRow">
           <label>
@@ -42,21 +68,18 @@ const Register = (props) => {
           </label>
         </div>
         <div className="formRow">
-          {" "}
           <label>
             First Name:
             <input type="name" placeholder="First Name" name="firstName" />
           </label>
         </div>
         <div className="formRow">
-          {" "}
           <label>
             Last Name:
             <input type="name" placeholder="Last Name" name="lastName" />
           </label>
         </div>
         <div className="formRow">
-          {" "}
           <label>
             Date of Birth:
             <input type="date" name="dateOfBirth" />
@@ -65,21 +88,23 @@ const Register = (props) => {
         <div className="formRow">
           <label>
             Gender:
-            <select>
-              <option value="" disabled selected>
+            <select defaultValue={"DEFAULT"}>
+              <option value="DEFAULT" disabled>
                 Select
               </option>
-              <option value="0">Female</option>
-              <option value="1">Male</option>
-              <option value="2">Other</option>
+              {genders.map((gender, index) => (
+                <option key={index} value={index}>
+                  {gender}
+                </option>
+              ))}
             </select>
           </label>
         </div>
         <div className="formRow">
           <label>
             Location:
-            <input type="name" name="location" placeholder="Town" />
-            <input type="name" name="postcode" placeholder="Postcode" />
+            <input type="name" name="location" placeholder="town" />
+            <input type="name" name="postcode" placeholder="postcode" />
           </label>
         </div>
         <div className="formRow">
@@ -91,60 +116,58 @@ const Register = (props) => {
         <div className="formRow">
           <label>
             Smokes?:
-            <select>
-              <option value="" disabled selected>
+            <select defaultValue={"DEFAULT"} name="smokes">
+              <option value="DEFAULT" disabled>
                 Select
               </option>
-              <option value="1">Yes</option>
-              <option value="2">Socially</option>
-              <option value="0">No</option>
+              {smokes.map((smokes, index) => (
+                <option key={index} value={index}>
+                  {smokes}
+                </option>
+              ))}
             </select>
           </label>
         </div>
         <div className="formRow">
           <label>
             Do you have any kids?:
-            <select>
-              <option value="" disabled selected>
+            <select defaultValue={"DEFAULT"} name="haveKids">
+              <option value="DEFAULT" disabled>
                 Select
               </option>
-              <option value="1">Yes</option>
               <option value="0">No</option>
+              <option value="1">Yes</option>
+              <option value="2">Prefer not to say</option>
             </select>
           </label>
         </div>
         <div className="formRow">
           <label>
             Do you want kids in the future?
-            <select>
-              <option value="" disabled selected>
+            <select defaultValue={"DEFAULT"} name="wantKids">
+              <option value="DEFAULT" disabled>
                 Select
               </option>
-              <option value="0">No</option>
-              <option value="1">Yes</option>
-              <option value="2">Not sure</option>
+              {wantKids.map((kids, index) => (
+                <option key={index} value={index}>
+                  {kids}
+                </option>
+              ))}
             </select>
           </label>
         </div>
         <div className="formRow">
           <label>
             Religion:
-            <select>
-              <option value="" disabled selected>
+            <select defaultValue={"DEFAULT"} name="religion">
+              <option value="DEFAULT" disabled>
                 Select
               </option>
-              <option value="0">African Traditional & Diasporic</option>
-              <option value="1">Agnostic</option>
-              <option value="2">Atheist</option>
-              <option value="3">Buddhism</option>
-              <option value="4">Chinese traditional religion</option>
-              <option value="5">Christianity</option>
-              <option value="6">Hinduism</option>
-              <option value="7">Islam</option>
-              <option value="8">Judaism</option>
-              <option value="9">Sikhism</option>
-              <option value="10">Spiritism</option>
-              <option value="11">Other</option>
+              {religions.map((religion, index) => (
+                <option key={index} value={index}>
+                  {religion}
+                </option>
+              ))}
             </select>
           </label>
         </div>
---
+++ src/config/formConfig.js
@@ -0,0 +1,60 @@
+import Joi from "joi";
+
+export const religions = [
+  "Prefer not to say",
+  "African Traditional & Diasporic",
+  "Agnostic",
+  "Atheist",
+  "Buddhism",
+  "Chinese traditional religion",
+  "Christianity",
+  "Hinduism",
+  "Islam",
+  "Judaism",
+  "Sikhism",
+  "Spiritism",
+  "Other",
+];
+
+export const wantKids = [
+  "Prefer not to say",
+  "Do not want kids",
+  "Not sure yet",
+  "Open to kids",
+  "Want kids",
+];
+
+export const smokes = ["No", "Sometimes", "Yes"];
+
+export const genders = [
+  "Prefer not to say",
+  "Female",
+  "Male",
+  "Non-binary",
+  "Transgender",
+  "Intersex",
+];
+
+const now = Date.now();
+const eigthteenYearsAgo = new Date(now - 1000 * 60 * 60 * 24 * 365 * 18);
+export const schema = {
+  email: Joi.string().email({ tlds: { allow: false } }),
+  firstName: Joi.string().alphanum().max(30).min(2),
+  lastName: Joi.string().alphanum().max(30).min(2),
+  password: Joi.string().min(4),
+  dateOfBirth: Joi.number().max(eigthteenYearsAgo.getTime()),
+  height: Joi.number().integer().positive(),
+  town: Joi.string().alphanum().max(30).min(2),
+  postcode: Joi.string().alphanum().max(7).min(2),
+};
+
+//Add required to validation
+
+export function joiDataReorder(details) {
+  const errorsMod = {};
+  details.forEach((error) => {
+    errorsMod[error.context.key] = error.message;
+  });
+
+  return errorsMod;
+}
