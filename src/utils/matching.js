6b1e58b7e8e410a75b0b1d144c33634d19ce05af
jessoy <81160034+jessoy@users.noreply.github.com>
2022-03-18 16:14:29 +0000

    add/remove search criteria functionality added

--- src/utils/matching.js
+++ src/utils/matching.js
@@ -52,3 +52,64 @@ export const ageCheck = (user1, user2) => {
     ? true
     : false;
 };
+
+export const genderFilter = (currentUser, user) => {
+  return (
+    currentUser.preferences.gender.includes(user.personalDetails.gender) &&
+    user.preferences.gender.includes(currentUser.personalDetails.gender)
+  );
+};
+
+export const heightFilter = (currentUser, user) => {
+  // checks if user1's height is more/less than user2's prefered min/max height
+
+  return heightCheck(currentUser, user) && heightCheck(user, currentUser);
+};
+
+export const heightCheck = (user1, user2) => {
+  return user1.personalDetails.height >= user2.preferences.height.min &&
+    user1.personalDetails.height <= user2.preferences.height.max
+    ? true
+    : false;
+};
+
+export const existingKidsFilter = (currentUser, user) => {
+  return (
+	(currentUser.personalDetails.kids === user.preferences.kidsAccepted &&
+	  user.personalDetails.kids === currentUser.preferences.kidsAccepted) ||
+	currentUser.personalDetails.kids === undefined ||
+	user.personalDetails.kids === undefined
+  );
+};
+
+export const openToKidsFilter = (currentUser, user) => {
+  console.log(
+	"cUser: " +
+	  currentUser.personalDetails.name.firstName +
+	  " (" +
+	  currentUser.preferences.lifeStyle.openToKids +
+	  ") " +
+	  " , user: " +
+	  user.personalDetails.name.firstName +
+	  " (" +
+	  user.preferences.lifeStyle.openToKids +
+	  ")"
+  );
+
+  if (
+	currentUser.preferences.lifeStyle.openToKids === 4 &&
+	user.preferences.lifeStyle.openToKids === 1
+  ) {
+	console.log("cUser wants kids, user don't");
+	return false;
+  } else if (
+	user.preferences.lifeStyle.openToKids === 4 &&
+	currentUser.preferences.lifeStyle.openToKids === 1
+  ) {
+	console.log("cUser doesn't want kids, user does");
+	return false;
+  } else {
+	console.log("kid want compatible");
+	return true;
+  }
+};
