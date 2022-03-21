cfe7d7cbaa75b6e5107ae8b99a21b092fd8ae6cf
jessoy <81160034+jessoy@users.noreply.github.com>
2022-03-21 12:32:49 +0000

    search updates - minro reformatting

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -5,45 +5,45 @@ import Messaging from "./messages/Messaging";
 import { useState } from "react";
 
 const Interface = (props) => {
-	const [screen, setScreen] = useState(0);
+  const [screen, setScreen] = useState(0);
 
-	const onLogin = (payload) => {
-		setScreen(1);
-		//save the new user data
-	};
+  const onLogin = (payload) => {
+    setScreen(1);
+    //save the new user data
+  };
 
-	const onMessageUpdate = (payload) => {
-		//do something useful
-	};
+  const onMessageUpdate = (payload) => {
+    //do something useful
+  };
 
-	return (
-		<>
-			<nav>
-				<button onClick={() => setScreen(0)}>Onboarding</button>
-				<button onClick={() => setScreen(1)}>Matches</button>
-				<button onClick={() => setScreen(2)}>Search</button>
-				<button onClick={() => setScreen(3)}>Messages</button>
-			</nav>
-			{screen === 0 && <Onboarding onLogin={onLogin} users={props.users} />}
+  return (
+    <>
+      <nav>
+        <button onClick={() => setScreen(0)}>Onboarding</button>
+        <button onClick={() => setScreen(1)}>Matches</button>
+        <button onClick={() => setScreen(2)}>Search</button>
+        <button onClick={() => setScreen(3)}>Messages</button>
+      </nav>
+      {screen === 0 && <Onboarding onLogin={onLogin} users={props.users} />}
 
-			{screen === 1 && (
-				<Matching onLikeUpdate={props.onLikeUpdate} users={props.users} />
-			)}
+      {screen === 1 && (
+        <Matching onLikeUpdate={props.onLikeUpdate} users={props.users} />
+      )}
 
-			{screen === 2 && <Search users={props.users} />}
+      {/* {screen === 2 && <Search users={props.users} />} */}
 
-			{screen === 3 && (
-				<Messaging
-					users={props.users}
-					messages={props.messages}
-					onMessageUpdate={onMessageUpdate}
-					addMessage={props.addMessage}
-					blockUserId={props.blockUserId}
-					deleteMessage={props.deleteMessage}
-				/>
-			)}
-		</>
-	);
+      {screen === 3 && (
+        <Messaging
+          users={props.users}
+          messages={props.messages}
+          onMessageUpdate={onMessageUpdate}
+          addMessage={props.addMessage}
+          blockUserId={props.blockUserId}
+          deleteMessage={props.deleteMessage}
+        />
+      )}
+    </>
+  );
 };
 
 export default Interface;
