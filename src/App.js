43ff041be02f28e332681f8c42ccf66a97ff121e
Ardo Al-Jaf <a.aljaf@gmail.com>
2022-03-21 14:21:52 +0000

    complete likeUpdate function

--- src/App.js
+++ src/App.js
@@ -12,22 +12,21 @@ const App = () => {
   const [userId, setUserId] = useState(getUniqueId(16));
   console.log(userId);
 
-//   //  set the state from the disk
-//   useEffect(() => {
-//     const data = getData();
-//     if (data.users && data.messages) {
-//       setUsers(data.users);
-//       setMessages(data.messages);
-//     }
-//   }, []);
-//   // when the state changes, save the changes to the disk
-//   useEffect(() => {
-//     storeData("users", users);
-//   }, [users]);
-//   useEffect(() => {
-//     storeData("messages", messages);
-//   }, [messages]);
-
+  //   //  set the state from the disk
+  //   useEffect(() => {
+  //     const data = getData();
+  //     if (data.users && data.messages) {
+  //       setUsers(data.users);
+  //       setMessages(data.messages);
+  //     }
+  //   }, []);
+  //   // when the state changes, save the changes to the disk
+  //   useEffect(() => {
+  //     storeData("users", users);
+  //   }, [users]);
+  //   useEffect(() => {
+  //     storeData("messages", messages);
+  //   }, [messages]);
 
   // Adds the current user ID to the blocked array in the data
   const blockUserId = (fId) => {
@@ -37,31 +36,34 @@ const App = () => {
     setUsers(usersCopy);
   };
 
-	const addMessage = (payload) => {
-		const copy = [...messages];
-		copy.push(payload);
-		setMessages(copy);
-		console.log(copy);
-	};
+  const addMessage = (payload) => {
+    const copy = [...messages];
+    copy.push(payload);
+    setMessages(copy);
+    console.log(copy);
+  };
 
-	const onLikeUpdate = (user, boolean) => {
-		addMessage({
-			toUserId: user.userId,
-			fromUserId: userId,
-			messageId: getUniqueId(16),
-			content: "",
-			sendTimestamp: 0,
-			read: false,
-			blocked: false,
-		});
-		// const usersCopy = [...users];
-		// if (boolean) {
-		//   usersCopy.currentUser.likes.push(user.userId);
-		//   usersCopy.currentUser.seen.push(user.userId);
-		// }
-		// check if users Match -> add respective IDs in respective {match}
-	};
-
+  const onLikeUpdate = (user) => {
+    const usersCopy = [...users];
+    usersCopy.currentUser.likes.push(user.userId); // push liked userId into currentUser's [liked]
+    if (usersCopy.user.likes.includes(currentUser.userId)) {
+      // if current user is also liked by user..
+      usersCopy.currentUser.matches.push(user.userId);
+      usersCopy.user.matches.push(currentUser.userId); // push eachother's id's into their respective [matches]
+      addMessage({
+        // & start convo
+        toUserId: user.userId,
+        fromUserId: userId,
+        messageId: getUniqueId(16),
+        content: "",
+        sendTimestamp: 0,
+        read: false,
+        blocked: false,
+      });
+      //insert notification function if desired
+    }
+    setUsers(usersCopy);
+  };
 
   const deleteMessage = (messageId) => {
     const messagesCopy = [...messages];
@@ -70,38 +72,38 @@ const App = () => {
     setMessages(messagesCopy);
   };
 
-	const addUser = async (newUser) => {
-		console.log(newUser);
-		const usersCopy = [...users];
-		// const coords = await getLngLat(newUser.personalDetails.location.postCode);
-		// newUser.personalDetails.location.longitude = coords.longitude;
-		// newUser.personalDetails.location.latitude = coords.latitude;
-		usersCopy.push(newUser);
-		console.log(usersCopy);
-		setUsers(usersCopy);
-	};
+  const addUser = async (newUser) => {
+    console.log(newUser);
+    const usersCopy = [...users];
+    // const coords = await getLngLat(newUser.personalDetails.location.postCode);
+    // newUser.personalDetails.location.longitude = coords.longitude;
+    // newUser.personalDetails.location.latitude = coords.latitude;
+    usersCopy.push(newUser);
+    console.log(usersCopy);
+    setUsers(usersCopy);
+  };
 
-	return (
-		<>
-			<button
-				onClick={() => {
-					localStorage.clear();
-				}}
-			>
-				Clear localStorage
-			</button>
-			<Interface
-				users={users}
-				messages={messages}
-				addMessage={addMessage}
-				onLikeUpdate={onLikeUpdate}
-				blockUserId={blockUserId}
-				addUser={addUser}
-				newUserId={userId}
-				deleteMessage={deleteMessage}
-			/>
-		</>
-	);
+  return (
+    <>
+      <button
+        onClick={() => {
+          localStorage.clear();
+        }}
+      >
+        Clear localStorage
+      </button>
+      <Interface
+        users={users}
+        messages={messages}
+        addMessage={addMessage}
+        onLikeUpdate={onLikeUpdate}
+        blockUserId={blockUserId}
+        addUser={addUser}
+        newUserId={userId}
+        deleteMessage={deleteMessage}
+      />
+    </>
+  );
 };
 
 export default App;
