aaaca35e0e33fe56e44339624e8832a4f3579446
Frederick Horne <frederickhorne@Fredericks-MBP.broadband>
2022-03-24 13:00:44 +0000

    block user updated

--- src/components/messages/Conversations.jsx
+++ src/components/messages/Conversations.jsx
@@ -6,18 +6,25 @@ const Conversations = (props) => {
 	const messages = useSelector((state) => state.messagingReducer.messages);
 
 	// Iterate through the messages data setting the keys for a new object to be user ID of the existing user and then saving that info in the entries object
+	// const result = {};
+	// messages.forEach((message) => {
+	// 	const existing = result[message.fromUserId]
+	// 		? result[message.fromUserId]
+	// 		: [];
+	// 	result[message.fromUserId] = [...existing, message];
+	// });
 	const result = {};
 	messages.forEach((message) => {
-		const existing = result[message.fromUserId]
-			? result[message.fromUserId]
-			: [];
-		result[message.fromUserId] = [...existing, message];
+		const existing = result[message.foreignId] ? result[message.foreignId] : [];
+		result[message.foreignId] = [...existing, message];
 	});
+	console.log(result);
+
 	const entries = Object.entries(result);
 
 	// Filter through the entries object returning only the conversations which do not match (not blocked)
 	const filteredConversations = entries.filter((conversation) => {
-		return !users[0].blocked.includes(Number(conversation[0]));
+		return !users[0].blocked.includes(Number(conversation[0])); //TBD must not be hard coded!!
 	});
 
 	return (
