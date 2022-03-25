a3a027f998facfd4337f5f0c8d29e1bca2cd4723
Ardo Al-Jaf <a.aljaf@gmail.com>
2022-03-25 17:03:02 +0000

    matching reducers local storage

--- src/components/matching/matchedComponents/UserImage.jsx
+++ src/components/matching/matchedComponents/UserImage.jsx
@@ -1,5 +1,6 @@
 const UserImage = (props) => {
-  return <div className="userImage">Image of user.</div>;
+  // return <div className="userImage">Image of user.</div>;
+  return <img src={props.image} alt="Image of user" />;
 };
 
 export default UserImage;
