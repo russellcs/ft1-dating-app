ad714cb49c33d96b90c4f90a3a02f6ec68703b1a
Frederick Horne <frederickhorne@fredericks-macbook-pro.home>
2022-03-27 15:49:47 +0100

    styling added with Bootstrap

--- src/components/messages/Message.jsx
+++ src/components/messages/Message.jsx
@@ -14,21 +14,30 @@ const Message = (props) => {
 	};
 
 	return (
-		<div style={{ margin: "20px" }}>
-			{message.content}
-			<button onClick={isLikeClicked}>&#9829;</button>
-			{likeClicked && <p>Message liked</p>}
-			<button
-				onClick={() => {
-					dispatch({
-						type: types.DELETE_MESSAGE,
-						payload: message.messageId,
-					});
-				}}
-				style={{ display: "block" }}
-			>
-				Delete message
-			</button>
+		<div style={{ width: "80%" }} className="m-4">
+			<p className="bg-white py-1 px-3 rounded shadow-sm">{message.content}</p>
+			<div className="position-relative d-flex">
+				<button
+					className="btn btn-sm btn-outline-dark me-3"
+					onClick={isLikeClicked}
+				>
+					&#9829;
+				</button>
+				{likeClicked && <p className="mb-0">Message liked</p>}
+
+				<button
+					className="btn btn-sm btn-dark position-absolute top-0 end-0"
+					onClick={() => {
+						dispatch({
+							type: types.DELETE_MESSAGE,
+							payload: message.messageId,
+						});
+					}}
+					style={{ display: "block" }}
+				>
+					Delete message
+				</button>
+			</div>
 		</div>
 	);
 };
