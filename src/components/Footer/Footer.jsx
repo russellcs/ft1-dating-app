d4f8051a904ce56128949ccb928284743b01a317
jessoy <81160034+jessoy@users.noreply.github.com>
2022-03-29 16:04:59 +0100

    reformatting footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,55 +2,17 @@ import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <footer className="fixed-bottom footer-59391">
+    <footer>
       <div className="container">
-        <div className="row mb-5">
-          <div className="col-md-4">
-            <div className="site-logo">
-              <a href="#">Sparks</a>
-            </div>
-          </div>
-        </div>
 
-        {/* <div className="row mb-5"> */}
-          {/* <div className="col-md-6 ">
-              <ul className="nav-links list-unstyled nav-left">
-                <li>
-                  <a href="#">Privacy</a>
-                </li>
-                <li>
-                  <a href="#">Policy</a>
-                </li>
-              </ul>
-            </div> */}
-          {/* <div className="col-md-6 text-md-right">
-              <ul className="nav-links list-unstyled nav-right">
-                <li>
-                  <a href="#">Home</a>
-                </li>
-                <li>
-                  <a href="#">Our works</a>
-                </li>
-                <li>
-                  <a href="#">About</a>
-                </li>
-                <li>
-                  <a href="#">Blog</a>
-                </li>
-                <li>
-                  <a href="#">Contact</a>
-                </li>
-              </ul>
-            </div> */}
-        {/* </div> */}
-        <div className="row">
-          <div className="col ">
-            <div className="copyright">
-              <p>
-                <small>Copyright 2022. All Rights Reserved.</small>
-              </p>
-            </div>
+          <div className="site-logo">
+            <a href="#">Sparks</a>
           </div>
+
+        <div className="copyright">
+          <p>
+            <small>Copyright 2022. All Rights Reserved.</small>
+          </p>
         </div>
       </div>
     </footer>
