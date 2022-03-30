b9dc6510ea4191935382d21e47946a767948fd34
Frederick Horne <frederickhorne@fredericks-macbook-pro.home>
2022-03-30 17:53:11 +0200

    added user img and name to each conversation

--- src/components/messages/UserImage.jsx
+++ src/components/messages/UserImage.jsx
@@ -1,8 +1,16 @@
 import { useSelector } from "react-redux";
+import { getIndexById } from "../../utils/matching";
 
-const UserImage = () => {
-	// const userImage = useSelector((state) => state.)
-	return <img src="" alt="user" />;
+const UserImage = (props) => {
+	const users = useSelector((state) => state.matching.users);
+	const index = getIndexById(props.foreignId, users);
+	return (
+		<img
+			className="w-25 rounded-circle ms-2"
+			src={users[index].personalDetails.selfie.image}
+			alt="user"
+		/>
+	);
 };
 
 export default UserImage;
