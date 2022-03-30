4e0c053132b6f5c6a2f3cb34b5ba2b883f3fa9ff
russelljumpdigital <77699151+russelljumpdigital@users.noreply.github.com>
2022-03-30 16:59:27 +0100

    all

--- src/components/messages/Message.jsx
+++ src/components/messages/Message.jsx
@@ -3,42 +3,42 @@ import { useDispatch } from "react-redux";
 import { types } from "../../redux/types";
 
 const Message = (props) => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const [likeClicked, setLikeClicked] = useState(false);
-	const { message } = props;
+  const [likeClicked, setLikeClicked] = useState(false);
+  const { message } = props;
 
-	// Toggles the Like button
-	const isLikeClicked = () => {
-		setLikeClicked(likeClicked === false ? true : false);
-	};
+  // Toggles the Like button
+  const isLikeClicked = () => {
+    setLikeClicked(likeClicked === false ? true : false);
+  };
 
-	return (
-		<div style={{ width: "80%" }} className="m-4">
-			<p className="bg-white py-1 px-3 rounded shadow-sm">{message.content}</p>
-			<div className="position-relative d-flex">
-				<button
-					className="btn btn-sm btn-outline-dark me-3"
-					onClick={isLikeClicked}
-				>
-					&#9829;
-				</button>
-				{likeClicked && <p className="mb-0">Message liked</p>}
+  return (
+    <div style={{ width: "80%" }} className="m-4">
+      <p className="bg-white py-1 px-3 rounded shadow-sm">{message.content}</p>
+      <div className="position-relative d-flex">
+        <button
+          className="btn btn-sm btn-outline-dark me-3"
+          onClick={isLikeClicked}
+        >
+          &#9829;
+        </button>
+        {likeClicked && <p className="mb-0">Message liked</p>}
 
-				<button
-					className="btn btn-sm btn-dark position-absolute top-0 end-0"
-					onClick={() => {
-						dispatch({
-							type: types.DELETE_MESSAGE,
-							payload: message.messageId,
-						});
-					}}
-				>
-					Delete message
-				</button>
-			</div>
-		</div>
-	);
+        <button
+          className="btn btn-sm btn-dark position-absolute top-0 end-0"
+          onClick={() => {
+            dispatch({
+              type: types.DELETE_MESSAGE,
+              payload: message.messageId,
+            });
+          }}
+        >
+          Delete message
+        </button>
+      </div>
+    </div>
+  );
 };
 
-export default Message;
+export default Mdessage;
