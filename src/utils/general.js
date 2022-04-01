59fca29fe0665a0818c4375bca7aae23ba395ff6
Ardo Al-Jaf <a.aljaf@gmail.com>
2022-04-01 13:26:53 +0100

    clean up

--- src/utils/general.js
+++ src/utils/general.js
@@ -1,67 +1,20 @@
-// import messages from "./mock";
-import axios from "axios";
-
-// console.log(messages);
-
-export const getLngLat = async (postcode) => {
-  try {
-    const result = await axios.get(
-      "https://api.postcodes.io/postcodes/ " + postcode
-    );
-
-    return {
-      latitude: result.data.result.latitude,
-      longitude: result.data.result.longitude,
-    };
-  } catch (error) {
-    return {
-      latitude: 0,
-      longitude: 0, //change to london
-    };
-  }
+export const getUserById = (id, users) => {
+  return users.find(({ userId }) => userId === id);
 };
 
-// Haversine function - from stack overflow
-function degreesToRadians(degrees) {
-  return (degrees * Math.PI) / 180;
-}
-
-function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
-  const earthRadiusKm = 6371;
-
-  const dLat = degreesToRadians(lat2 - lat1);
-  const dLon = degreesToRadians(lon2 - lon1);
-
-  lat1 = degreesToRadians(lat1);
-  lat2 = degreesToRadians(lat2);
-
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-  return earthRadiusKm * c;
-}
-
-export const findDistanceFromLongsAndLats = (location1, location2) => {
-  const lat1 = location1.latitude;
-  const lat2 = location2.latitude;
-  const lon1 = location1.longitude;
-  const lon2 = location2.longitude;
-  // console.log(location2);
-  return distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2);
+export const getIndexByEmailAndPassword = (emailTest, passwordTest, users) => {
+  console.log(emailTest, passwordTest);
+  return users.findIndex(
+    (user) =>
+      user.login.email === emailTest && user.login.password === passwordTest
+  );
 };
 
-export function getUniqueId(length) {
-  // return Math.round(Math.random() * 10000000000);
-  const now = Date.now().toString();
-  let uniqueId = "";
-  const chars =
-    "ABCDEFGHIJUKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
-  const charsLength = chars.length;
+export const getIndexById = (id, users) => {
+  // console.log(users, id);
+  return users.findIndex(({ userId }) => userId === id);
+};
 
-  for (let index = 0; index < length - now.length; index++) {
-    uniqueId += chars.charAt(Math.floor(Math.random() * charsLength));
-  }
-  return uniqueId + Date.now();
-}
+export const getMessageIndexById = (id, messages) => {
+  return messages.findIndex(({ messageId }) => messageId === id);
+};
