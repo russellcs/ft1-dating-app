5db8a1338d781ab546dc5846eebc2a86f23da03f
jessoy <81160034+jessoy@users.noreply.github.com>
2022-04-11 17:25:10 +0100

    get all users

--- src/App.js
+++ src/App.js
@@ -3,41 +3,51 @@ import Title from "./components/general/Title";
 import { useDispatch } from "react-redux";
 import { types } from "./redux/types/types";
 import { useSelector } from "react-redux";
-import { callAPI } from "./dataController/messages";
+import { callAPI as matchingCallAPI } from "./dataController/matching";
+import { callAPI as messagingCallAPI } from "./dataController/messages";
 import "./App.css";
 
 const App = () => {
-	const dispatch = useDispatch();
-	const loggedIn = useSelector((state) => state.general.loggedIn);
+  const dispatch = useDispatch();
+  const loggedIn = useSelector((state) => state.general.loggedIn);
 
-	callAPI(types.GET_USER_MESSAGES, { userId: 1 });
+  const getInitialData = async () => {
+    // await messagingCallAPI(types.GET_USER_MESSAGES, { userId: 1 });
+    const userData = await matchingCallAPI(types.GET_ALL_USERS);
 
-	return (
-		<>
-			<button
-				className="btn btn-primary"
-				onClick={() => {
-					localStorage.clear();
-				}}
-			>
-				Clear localStorage
-			</button>
+	// dispatch({type: types.})
 
-			{loggedIn === true && (
-				<button
-					className="btn log-out "
-					style={{ float: "right" }}
-					onClick={() => {
-						dispatch({ type: types.LOG_OUT });
-					}}
-				>
-					Log Out
-				</button>
-			)}
-			<Title />
-			<Interface />
-		</>
-	);
+    console.log("userdata", userData);
+  };
+
+  getInitialData();
+
+  return (
+    <>
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          localStorage.clear();
+        }}
+      >
+        Clear localStorage
+      </button>
+
+      {loggedIn === true && (
+        <button
+          className="btn log-out "
+          style={{ float: "right" }}
+          onClick={() => {
+            dispatch({ type: types.LOG_OUT });
+          }}
+        >
+          Log Out
+        </button>
+      )}
+      <Title />
+      <Interface />
+    </>
+  );
 };
 
 export default App;
