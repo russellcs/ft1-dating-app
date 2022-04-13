f771f539f3c060ff882bbe2135079183cbacdd01
TF <tf@tfs-imac.lan>
2022-04-13 09:38:29 +0100

    cleaned up some old register code, added types for dataControllers and notes for types pt 2

--- src/dataController/matching.js
+++ src/dataController/matching.js
@@ -1,24 +1,25 @@
 import { API_URL } from "../config/general";
 import { callAxios } from "./dynamicAxios";
+import { types } from "../redux/types/types";
 
 export const callAPI = async (type, payload) => {
   // console.log(type);
   switch (type) {
-    case "ADD_TO_SEEN":
+    case types.ADD_TO_SEEN:
       callAxios("post", API_URL + "/matching/seen", {
         user_id: payload.userId,
         foreign_id: payload.foreignUserId,
       });
       break;
 
-    case "ADD_TO_LIKES":
+    case types.ADD_TO_LIKES:
       callAxios("post", API_URL + "/matching/likes", {
         user_id: payload.userId,
         foreign_id: payload.foreignUserId,
       });
       break;
 
-    case "GET_ALL_USERS":
+    case types.GET_ALL_USERS:
       // console.log("about to get results");
       const result = await callAxios("get", API_URL + "/matching/0");
       // console.log(result);
