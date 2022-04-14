0b192b5d84d158e90fa2e56cfd6ee7202c55c896
Frederick Horne <frederickhorne@fredericks-macbook-pro.home>
2022-04-14 17:37:24 +0200

    final styling on matching page

--- src/components/matching/MatchedUser.jsx
+++ src/components/matching/MatchedUser.jsx
@@ -13,56 +13,54 @@ import UserTown from "./matchedComponents/UserTown";
 import { getAge } from "../../utils/general";
 import { API_URL } from "../../config/general";
 
-
 const MatchedUser = (props) => {
-  const { user } = props;
-  return (
-    <div>
-      <UserName name={user.personalDetails.name.firstName} />
+	const { user } = props;
+	return (
+		<div className="mb-2">
+			<UserName name={user.personalDetails.name.firstName} />
 
-      {/* <UserImage image={user.personalDetails.selfie.image} /> */}
-      <UserImage image={`${API_URL}/userImages/${user.userId}.jpg`} />
+			<UserImage image={`${API_URL}/userImages/${user.userId}.jpg`} />
 
-      <ul className="userDetails">
-        <li>
-          <UserGender gender={user.personalDetails.gender} />
-        </li>
-        <li>
-          <UserTown town={user.personalDetails.location.town} />
-        </li>
-        <li>
-          <UserAge age={getAge(user.personalDetails.dob)} />
-        </li>
-        <li>
-          <UserHeight height={user.personalDetails.height} />
-        </li>
-        <li>
-          <UserMarriage marriage={user.preferences.lifeStyle.marriage} />
-        </li>
-        <li>
-          <UserCasual casual={user.preferences.lifeStyle.casual} />
-        </li>
+			<ul className="userDetails">
+				<li>
+					<UserGender gender={user.personalDetails.gender} />
+				</li>
+				<li>
+					<UserTown town={user.personalDetails.location.town} />
+				</li>
+				<li>
+					<UserAge age={getAge(user.personalDetails.dob)} />
+				</li>
+				<li>
+					<UserHeight height={user.personalDetails.height} />
+				</li>
+				<li>
+					<UserMarriage marriage={user.preferences.lifeStyle.marriage} />
+				</li>
+				<li>
+					<UserCasual casual={user.preferences.lifeStyle.casual} />
+				</li>
 
-        <li>
-          <UserChildren haveKids={user.personalDetails.kids} />
-        </li>
+				<li>
+					<UserChildren haveKids={user.personalDetails.kids} />
+				</li>
 
-        <li>
-          <UserOpenToKids openToKids={user.preferences.lifeStyle.openToKids} />
-        </li>
-        {user.personalDetails.religion ? (
-          <li className="truncate">
-            <UserReligion religion={user.personalDetails.religion} />
-          </li>
-        ) : null}
-        {user.personalDetails.smokers ? (
-          <li>
-            <UserSmokes smoker={user.personalDetails.smokers} />
-          </li>
-        ) : null}
-      </ul>
-    </div>
-  );
+				<li>
+					<UserOpenToKids openToKids={user.preferences.lifeStyle.openToKids} />
+				</li>
+				{user.personalDetails.religion ? (
+					<li className="truncate">
+						<UserReligion religion={user.personalDetails.religion} />
+					</li>
+				) : null}
+				{user.personalDetails.smokers ? (
+					<li>
+						<UserSmokes smoker={user.personalDetails.smokers} />
+					</li>
+				) : null}
+			</ul>
+		</div>
+	);
 };
 
 export default MatchedUser;
--- src/components/matching/Matching.jsx
+++ src/components/matching/Matching.jsx
@@ -11,106 +11,106 @@ import { gsap } from "gsap";
 import { callAPI } from "../../dataController/matching";
 
 const Matching = () => {
-  const [currentResultIndex, setCurrentResultIndex] = useState(0);
+	const [currentResultIndex, setCurrentResultIndex] = useState(0);
 
-  const users = useSelector((state) => state.matching.users);
-  const matchingFilter = useSelector((state) => state.general.matchingFilter);
-  const currentUserId = useSelector((state) => state.general.currentUserId);
-  let currentUser = getUserById(currentUserId, users);
-  const dispatch = useDispatch();
+	const users = useSelector((state) => state.matching.users);
+	const matchingFilter = useSelector((state) => state.general.matchingFilter);
+	const currentUserId = useSelector((state) => state.general.currentUserId);
+	let currentUser = getUserById(currentUserId, users);
+	const dispatch = useDispatch();
 
-  // Creates duplicate of all users, filtered and sorted according to compatabilities with current user.
-  let usersCopy = [...users];
-  usersCopy.splice(
-    usersCopy.findIndex((user) => user === currentUser),
-    1
-  );
-  let filteredUsers = filteringUsers(usersCopy, currentUser, matchingFilter);
-  let sortedUsers = sortingUsers(filteredUsers, currentUser);
-  let userForReview = sortedUsers[currentResultIndex];
+	// Creates duplicate of all users, filtered and sorted according to compatabilities with current user.
+	let usersCopy = [...users];
+	usersCopy.splice(
+		usersCopy.findIndex((user) => user === currentUser),
+		1
+	);
+	let filteredUsers = filteringUsers(usersCopy, currentUser, matchingFilter);
+	let sortedUsers = sortingUsers(filteredUsers, currentUser);
+	let userForReview = sortedUsers[currentResultIndex];
 
-  // update current user's likes, check if they like eachother, load next user for review.
-  const onLike = (user) => {
-    callAPI("ADD_TO_LIKES", {
-      userId: currentUser.userId,
-      foreignUserId: user.userId,
-    });
+	// update current user's likes, check if they like eachother, load next user for review.
+	const onLike = (user) => {
+		callAPI("ADD_TO_LIKES", {
+			userId: currentUser.userId,
+			foreignUserId: user.userId,
+		});
 
-    const usersToAddToLikes = { user, currentUser };
-    dispatch({ type: types.ADD_TO_LIKES, payload: usersToAddToLikes });
-    dispatch({
-      type: types.UPDATE_MATCHES,
-      payload: { seenUserId: user.userId, currentUserId: currentUser.userId },
-    });
-    //foreign ID convention not seenUser
+		const usersToAddToLikes = { user, currentUser };
+		dispatch({ type: types.ADD_TO_LIKES, payload: usersToAddToLikes });
+		dispatch({
+			type: types.UPDATE_MATCHES,
+			payload: { seenUserId: user.userId, currentUserId: currentUser.userId },
+		});
+		//foreign ID convention not seenUser
 
-    setCurrentResultIndex(currentResultIndex + 1);
-  };
+		setCurrentResultIndex(currentResultIndex + 1);
+	};
 
-  // load next user for review
-  const onPass = () => {
-    setCurrentResultIndex(currentResultIndex + 1);
-    gsap.fromTo(".btn-pass", { color: "#000" }, { color: "#fff", duration: 1 });
-  };
+	// load next user for review
+	const onPass = () => {
+		setCurrentResultIndex(currentResultIndex + 1);
+		gsap.fromTo(".btn-pass", { color: "#000" }, { color: "#fff", duration: 1 });
+	};
 
-  //Add user being reviewed to current user's seen array.
-  useEffect(() => {
-    if (currentResultIndex < sortedUsers.length) {
-      callAPI("ADD_TO_SEEN", {
-        userId: currentUser.userId,
-        foreignUserId: userForReview.userId,
-      });
+	//Add user being reviewed to current user's seen array.
+	useEffect(() => {
+		if (currentResultIndex < sortedUsers.length) {
+			callAPI("ADD_TO_SEEN", {
+				userId: currentUser.userId,
+				foreignUserId: userForReview.userId,
+			});
 
-      // dispatch({
-      //   type: types.ADD_TO_SEEN,
-      //   payload: {
-      //     seenUserId: userForReview.userId,
-      //     currentUserId: currentUser.userId,
-      //   },
-      // });
-    }
-  }, [
-    currentResultIndex,
-    // currentUser.userId,
-    // sortedUsers.length,
-    // userForReview.userId,
-    // dispatch,
-  ]);
+			// dispatch({
+			//   type: types.ADD_TO_SEEN,
+			//   payload: {
+			//     seenUserId: userForReview.userId,
+			//     currentUserId: currentUser.userId,
+			//   },
+			// });
+		}
+	}, [
+		currentResultIndex,
+		// currentUser.userId,
+		// sortedUsers.length,
+		// userForReview.userId,
+		// dispatch,
+	]);
 
-  return (
-    <>
-      <Search />
+	return (
+		<>
+			<Search />
 
-      {currentResultIndex < sortedUsers.length ? (
-        <div className="container">
-          <div className="userCard row card container-sm shadow">
-            <MatchedUser user={userForReview} />
-          </div>
-          <div className="row my-2">
-            <button
-              className="col btn btn-love btn-round btn-lg ms-3 me-1"
-              onClick={() => onLike(userForReview)}
-            >
-              Like
-            </button>
-            <button
-              className="col btn btn-pass btn-round btn-lg ms-1 me-3"
-              onClick={() => onPass()}
-            >
-              Pass
-            </button>
-          </div>
-        </div>
-      ) : (
-        <div>
-          <p>
-            You've seen all potential matches in current search parameters, try
-            again later or be less picky!
-          </p>
-        </div>
-      )}
-    </>
-  );
+			{currentResultIndex < sortedUsers.length ? (
+				<div className="container">
+					<div className="userCard row card container-sm shadow">
+						<MatchedUser user={userForReview} />
+					</div>
+					<div className="row my-2">
+						<button
+							className="col btn btn-pass btn-round btn-lg ms-3 me-2 mt-2"
+							onClick={() => onPass()}
+						>
+							Pass
+						</button>
+						<button
+							className="col btn btn-success btn-round btn-lg ms-2 me-3 mt-2"
+							onClick={() => onLike(userForReview)}
+						>
+							Like
+						</button>
+					</div>
+				</div>
+			) : (
+				<div>
+					<p>
+						You've seen all potential matches in current search parameters, try
+						again later or be less picky!
+					</p>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default Matching;
