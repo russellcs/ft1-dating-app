6e84bbea5fee01fffe9f7f8da7f604d720581a11
Frederick Horne <frederickhorne@fredericks-macbook-pro.home>
2022-04-14 14:56:04 +0200

    storage re-added

--- src/components/messages/UserImage.jsx
+++ src/components/messages/UserImage.jsx
@@ -3,15 +3,15 @@ import { getIndexById } from "../../utils/general";
 import { API_URL } from "../../config/general";
 
 const UserImage = (props) => {
-  const users = useSelector((state) => state.matching.users);
-  const index = getIndexById(props.foreignId, users);
-  return (
-    <img
-      className="w-50 rounded-circle ms-2 shadow-lg"
-      src={`${API_URL}/userImages/${props.foreignId}.jpg`} 
-      alt="depiction of user"
-    />
-  );
+	const users = useSelector((state) => state.matching.users);
+	const index = getIndexById(props.foreignId, users);
+	return (
+		<img
+			className="w-50 rounded-circle ms-2 shadow-lg"
+			src={`${API_URL}/userImages/${props.foreignId}.jpg`}
+			alt="depiction of user"
+		/>
+	);
 };
 
 export default UserImage;
